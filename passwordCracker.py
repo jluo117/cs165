865203fd49c077ddd9dc5446e44fb64309f25811
james luo <luojames52@gmail.com>
2019-01-09 20:59:55 -0800

    4 digits

--- passwordCracker.py
+++ passwordCracker.py
@@ -6,7 +6,7 @@ def hashValue(inputStr):
 	myHash.digest()
 	return myHash.hexdigest()
 def main():
-	rawText = "xyz"
+	rawText = "zyaj"
 	for i in range(1000):
 		rawText = hashValue(rawText)
 	for i in range (27):
@@ -15,12 +15,14 @@ def main():
 			jASCII = j + 97
 			for k in range(27):
 				kASCII =  k + 97
-				currentChar = chr(iASCII) + chr(jASCII) + chr(kASCII)
-				for i in range(1000):
-					currentChar = hashValue(currentChar)
-				if currentChar == rawText:
-					print("done")
-					return
+				for l in range(27):
+					lASCII = l + 97
+					currentChar = chr(iASCII) + chr(jASCII) + chr(kASCII) + chr(lASCII)
+					for i in range(1000):
+						currentChar = hashValue(currentChar)
+					if currentChar == rawText:
+						print("done")
+						return
 start_time = time.time()
 main()
 print("--- %s seconds ---" % (time.time() - start_time))

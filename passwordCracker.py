71e80839ab755bf713d7466f65e4d39c2c01653f
james luo <luojames52@gmail.com>
2019-01-11 14:20:49 -0800

    following algo

--- passwordCracker.py
+++ passwordCracker.py
@@ -1,24 +1,76 @@
-import 	hashlib
-import 	time
-def hashValue(inputStr):
+from passlib.hash import md5_crypt
+import  hashlib
+import sys
+import threading
+salt = "hfT7jp2q"
+passwordList = []
+solved = False
+def ALTsum(inputStr):
 	myHash = hashlib.md5()
-	myHash.update(str.encode(inputStr))
+	myHash.update(str.encode(inputStr + salt + inputStr))
 	myHash.digest()
 	return myHash.hexdigest()
-def main():
-	for i in range (26):
-		iASCII = i + 97
-		for j in range(26):
-			jASCII = j + 97
-			for k in range(26):
-				kASCII = k + 97
-				for l in range(26):
-					lASCII = l + 97
-					currentChar = chr(iASCII) + chr(jASCII) + chr(kASCII) + chr(lASCII)
-					HashChar = chr(iASCII) + chr(jASCII) + chr(kASCII) + chr(lASCII)
-					for i in range(1000):
-						HashChar = hashValue(HashChar)
-					print(currentChar + " " + HashChar)					
-start_time = time.time()
-main()
-print("--- %s seconds ---" % (time.time() - start_time))
+
+def interSum(password,altSum):
+	intersum = password + magic + salt
+	for i in range(len(altSum)):
+		intersum += altSum
+	myBinary = str(format(len(password), "b"))
+	for i in myBinary:
+		if i != 0:
+			intersum += password[0]
+		else:
+			interSum += chr(ord('a') - 97)
+	myHash = hashlib.md5()
+	myHash.update(str.encode(interSum))
+	myHash.digest() 
+	return myHash.hexdigest()
+
+def fastHash(intermedite,password,salt):
+	theHash = None
+	for i in range(1000):
+		if i % 2 == 0:
+			theHash = intermedite
+		else:
+			theHash = password
+		if i % 3 != 0:
+			theHash += salt
+		if i % 7 != 0:
+			theHash += password
+		if i % 2 == 0:
+			theHash += password
+		else:
+			theHash += intermedite
+		myHash = hashlib.md5()
+		myHash.update(str.encode(theHash))
+		myHash.digest()
+		intermedite = myHash.hexdigest()
+def generatePW():
+	for i in range(97,123):
+		curPassWord = chr(i)
+		passwordList.append(curPassWord)
+		for j in range(97,123):
+			curPassWord += chr(j)
+			passwordList.append(curPassWord)
+			for k in range(97,123):
+				curPassWord += chr(k)
+				passwordList.append(curPassWord)
+				for l in range(97,123):
+					curPassWord += chr(l)
+					passwordList.append(curPassWord)
+					for m in range(97,123):
+						curPassWord += chr(m)
+						passwordList.append(curPassWord)
+						for n in range(97,123):
+							curPassWord += chr(n)
+							passwordList.append(curPassWord)
+							for o in range(97,123):
+								curPassWord += chr(o)
+								passwordList.append(curPassWord)
+								for p in range(97,123):
+									curPassWord += chr(p)
+									passwordList.append(curPassWord)
+									print(passwordList)
+									if solved:
+										sys.exit()
+print(chr(ord('a') - 97))

a921ff5ea0073c306e8af4a129c5f37eb8b96210
james luo <luojames52@gmail.com>
2019-01-11 12:00:42 -0800

    maualbuild

--- passwordCracker.py
+++ passwordCracker.py
@@ -1,11 +1,59 @@
-import 	hashlib
+import  hashlib
 import 	time
-def hashValue(inputStr):
+salt = "hfT7jp2q"
+pw = "ti"
+charTest = 't'
+magic = "$1$"
+pwlen = len(pw)
+def ALTsum(inputStr):
 	myHash = hashlib.md5()
-	myHash.update(str.encode(inputStr))
+	myHash.update(str.encode(inputStr + salt + inputStr))
 	myHash.digest()
 	return myHash.hexdigest()
-def main():
+
+def interSum(password,altSum):
+	intersum = password + magic + salt
+	for i in range(len(altSum)):
+		intersum += altSum
+	myBinary = str(format(len(password), "b"))
+	for i in myBinary:
+		if i == 0:
+			
+	myHash = hashlib.md5()
+	myHash.update(str.encode(interSum))
+	myHash.digest() 
+	return myHash.hexdigest()
+
+
+def isKthBitSet(n, k): 
+    if n & (1 << (k - 1)): 
+        return True
+    else: 
+        return  False
+def fastHash(intermedite,password,salt):
+	theHash = None
+	for i in range(1000):
+		if i % 2 == 0:
+			theHash = intermedite
+		else:
+			theHash = password
+		if i % 3 != 0:
+			theHash += salt
+		if i % 7 != 0:
+			theHash += password
+		if i % 2 == 0:
+			theHash += password
+		else:
+			theHash += intermedite
+		myHash = hashlib.md5()
+		myHash.update(str.encode(theHash))
+		myHash.digest()
+		intermedite = myHash.hexdigest()
+
+def cryptoHash(password):
+	return fastHash(interSum(password,ALTsum(password)),password,salt) 
+
+def CrackHash(value):
 	for i in range (26):
 		iASCII = i + 97
 		for j in range(26):
@@ -14,11 +62,15 @@ def main():
 				kASCII = k + 97
 				for l in range(26):
 					lASCII = l + 97
-					currentChar = chr(iASCII) + chr(jASCII) + chr(kASCII) + chr(lASCII)
-					HashChar = chr(iASCII) + chr(jASCII) + chr(kASCII) + chr(lASCII)
-					for i in range(1000):
-						HashChar = hashValue(HashChar)
-					print(currentChar + " " + HashChar)					
+					for m in range (26):
+						mASCII = m + 97
+						for n in range (26):
+							nASCII = n + 97
+							currentChar = chr(iASCII) + chr(jASCII) + chr(kASCII) + chr(lASCII) + chr(mASCII) + chr(nASCII)
+							HashChar = chr(iASCII) + chr(jASCII) + chr(kASCII) + chr(lASCII) + chr(mASCII) + chr(nASCII)
+							for i in range(1000):
+								HashChar = hashValue(HashChar)
+											
 start_time = time.time()
-main()
+print (format(8, "b"))
 print("--- %s seconds ---" % (time.time() - start_time))

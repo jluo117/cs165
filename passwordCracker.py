3441167b0732cb109a869a84b9a01231a0ddb427
james luo <luojames52@gmail.com>
2019-01-17 13:41:49 -0800

    running on gcp

--- passwordCracker.py
+++ passwordCracker.py
@@ -4,13 +4,18 @@ import  hashlib
 import sys
 import threading
 import numpy as np
+import twilio
+from twilio.rest import Client
+import threading
+from queue import *
+solved = ["done"]
 CryptBase = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 md5CryptSwaps = [12, 6, 0, 13, 7, 1, 14, 8, 2, 15, 9, 3, 5, 10, 4, 11]
 salt = "hfT7jp2q"
 salt = salt
 magic = "$1$"
-passwordList = []
-solved = False
+passwordList = Queue()
+targetHash = None
 _c_digest_offsets = (
     (0, 3), (5, 1), (5, 3), (1, 2), (5, 1), (5, 3), (1, 3),
     (4, 1), (5, 3), (1, 3), (5, 0), (5, 3), (1, 3), (5, 1),
@@ -86,8 +91,8 @@ def reorder(fasthash):
 	bits = unsigned(0)
 	for i in md5CryptSwaps:
 		v |= fasthash.digest()[i] << bits
-		print(unsigned(fasthash.digest()[i]))
-		print(bin (fasthash.digest()[i]))
+		#print(unsigned(fasthash.digest()[i]))
+		#print(bin (fasthash.digest()[i]))
 		#print(v)
 		#print((fasthash.digest()[i]))
 		bits += 8
@@ -102,7 +107,7 @@ def aryToStr(ary):
 	returnStr = ""
 	for i in ary:
 		returnStr += i
-	return magic + salt + '$' + returnStr
+	return magic +  salt+ '$' + returnStr
 
 def genHash(password):
 	altsum = ALTsum(password)
@@ -113,29 +118,73 @@ def genHash(password):
 def generatePW():
 	for i in range(97,123):
 		curPassWord = chr(i)
-		passwordList.append(curPassWord)
+		passwordList.put(curPassWord)
 		for j in range(97,123):
 			curPassWord += chr(j)
-			passwordList.append(curPassWord)
+			passwordList.put(curPassWord)
 			for k in range(97,123):
 				curPassWord += chr(k)
-				passwordList.append(curPassWord)
+				passwordList.put(curPassWord)
 				for l in range(97,123):
 					curPassWord += chr(l)
-					passwordList.append(curPassWord)
+					passwordList.put(curPassWord)
 					for m in range(97,123):
 						curPassWord += chr(m)
-						passwordList.append(curPassWord)
+						passwordList.put(curPassWord)
 						for n in range(97,123):
 							curPassWord += chr(n)
-							passwordList.append(curPassWord)
+							passwordList.put(curPassWord)
 							for o in range(97,123):
 								curPassWord += chr(o)
-								passwordList.append(curPassWord)
+								passwordList.put(curPassWord)
 								for p in range(97,123):
 									curPassWord += chr(p)
-									passwordList.append(curPassWord)
-									print(passwordList)
+									passwordList.put(curPassWord)
 									if solved:
 										sys.exit()
-print(genHash("password"))
+def consumer_thread(targetHash):
+
+	while not len(solved) == 0:
+		#print(targetHash)
+		myPWD = passwordList.get()
+		#print(myPWD)
+		result = genHash(myPWD)
+		#print(result)
+		if str(result) == str(targetHash):
+			#print(result)
+			print(myPWD)
+			sendMsg(myPWD)
+			solved.pop()
+	sys.exit()
+
+def sendMsg(msg):
+    account_sid = "ACe704104c6f665965aeb765eea2a1502a"
+    auth_token = "2aa7630860a97a3fb9de4ab53a94abc4"
+
+    client = Client(account_sid, auth_token)
+    client.api.account.messages.create(
+        to="+14158109857",
+        from_="+15108228362",
+        body=msg,
+        )
+
+def main():
+	testHash = "$1$hfT7jp2q$B96oRTlE0yZWjRx7qoO920"
+	targetHash = testHash
+	t1 = threading.Thread( target = generatePW())
+	t1.start()
+	for i in range(7):
+		myThread = threading.Thread( target = consumer_thread(targetHash))
+		myThread.start()
+def notThreading():
+	targetHash = genHash("aaaaz")
+	#print(targetHash)
+	passwordList.put("asdasdasd")
+	passwordList.put("fdsafdsafsadf")
+	passwordList.put("aaaaz")
+	passwordList.put("fdsafdsafsadf")
+	myThread = threading.Thread( target = consumer_thread(targetHash))
+	myThread.start()
+	myThread2 = threading.Thread( target = consumer_thread(targetHash))
+	myThread2.start()
+main()
